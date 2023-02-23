7d383f9960c4eac33d4cc206fc4d3984c0180473
jubescraudo <125514328+jubescraudo@users.noreply.github.com>
2023-02-22 21:12:34 -0300

    Add files via upload

--- PAULO/2_variaveis.php
+++ PAULO/2_variaveis.php
@@ -1,8 +1,25 @@
 <?php
-$name = "teste"
+$name = "Maria";
 $age = 24;
 
 echo $name;
+echo "<br>";
 echo $age;
+echo "<hr>";
+echo "Booleano: ";
+$b = true;
+echo gettype($b);
+echo "<hr>";
 
+$nasc = 1994;
+echo gettype($nasc);
+echo "<hr>";
+$n1 = "teste";
+echo gettype($n1);
+echo "<hr>";
+$n2 = 5.5;
+echo gettype($n2);
+echo "<hr>";
+$n3 = '3.14';
+echo gettype($n3);
 ?>
