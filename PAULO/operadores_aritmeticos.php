263ea2d7bc642d46c2866ec9a32b5aaba50fecf9
jubescraudo <125514328+jubescraudo@users.noreply.github.com>
2023-02-24 21:06:53 -0300

    Add files via upload

---
+++ PAULO/operadores_aritmeticos.php
@@ -0,0 +1,21 @@
+<?php
+$a = 3;
+$b = 3;
+
+$soma = $a * $b;
+echo "$soma <hr>";
+
+$subtracao = $a - $b;
+echo "$subtracao <hr>";
+
+$divisao = $a / $b;
+echo "$divisao <hr>";
+
+$multiplicacao = $a * $b;
+echo "$multiplicacao <hr>";
+
+$dudafeia = $a % $b;
+echo "$dudafeia <hr>";
+
+ 
+?>
