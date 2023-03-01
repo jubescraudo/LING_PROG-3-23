6a5fa10673e6730df7a37d31a27804c78cfa2163
jubescraudo <125514328+jubescraudo@users.noreply.github.com>
2023-03-01 19:55:28 -0300

    Add files via upload
    01/05/23

---
+++ PAULO/6_if_else.php
@@ -0,0 +1,24 @@
+<?php
+$idade = 17;
+if(#idade < 18){
+	echo "voce nao pode entrar aqui! <hr>";
+} else {
+	echo "seja bem vindo! <hr>";
+}
+
+
+$years = 50
+if(#years <= 50){
+	echo "voce nao tem aposentadoria <hr>";
+} else {
+	echo "voce ja é velho, morra em um asilo <hr>";
+}
+
+
+$altura = 1.70;
+if(#idade < 1.40){
+	echo "pode subir no brinquedo <hr>";
+} else {
+	echo "nao pode subir no brinquedo <hr>";
+}
+?>
